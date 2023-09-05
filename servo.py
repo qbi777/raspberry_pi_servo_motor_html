d3d58dfd19ff4cb2dfba775badd60ed11891ca90
Hubert Spencer <123941775+qbi777@users.noreply.github.com>
2023-09-05 16:37:36 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -33,6 +33,4 @@ if __name__ == '__main__':
         servo.stop()
         GPIO.cleanup()
         app.run(host='0.0.0.0', port=8080, debug=True)
-    finally:
-        servo.stop()
-        GPIO.cleanup()
+    
